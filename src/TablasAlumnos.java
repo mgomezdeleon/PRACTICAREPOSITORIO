8abc85570d2823a8a6790dc3325fa86a4c561818
jose_ <jose_@Portatil-Jose>
2024-04-30 13:02:26 +0200

    Tablas de jose

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,22 @@
 
 public class TablasAlumnos {
-			public static void main(String[] args) {
+	
+	public static void main(String[] args) {
+		
 			System.out.println("Tablas de Multiplicar");
+
+			imprimirTablasDeMultiplicar();
+			   
 		}
+    public static void imprimirTablasDeMultiplicar() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabla de multiplicar del " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
 	}
 
 
