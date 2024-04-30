a6aae5b3aaf46c3b50e85f5e187ba2743f4b3921
Samuel <Samuel@DESKTOP-QKLF2B8>
2024-04-30 13:06:02 +0200

    Samuel

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -2,6 +2,16 @@
 public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
+			for(int i=0; i<11;i++) {
+				
+				System.out.println("Tabla del " + i);
+				for(int j=0;j<11;j++) {
+					
+					System.out.println(i + " x " + j + "= " + (i * j));
+					
+				}
+				System.out.println("**************");
+			}
 		}
 	}
 
