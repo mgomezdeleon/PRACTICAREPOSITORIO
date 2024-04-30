c89c73c012e9bd2a3586223323362e34bd4ba8e2
Marco <Marco@Nitro>
2024-04-30 13:09:43 +0200

    hecho

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -2,6 +2,13 @@
 public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println("Tabla de multiplicar del " + i + ":");
+	            for (int j = 1; j <= 10; j++) {
+	                System.out.println(i + " x " + j + " = " + (i * j));
+	            }
+	            System.out.println(); // Salto de línea para separar las tablas
+	        }
 		}
 	}
 
