ba39dda3aae4cdb2d382234a44d91cc1abbed06a
JanoT19 <janotoralsanmiguel2004@gmail.com>
2024-04-30 17:09:59 +0200

    prueba Jano

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,12 @@
-
 public class TablasAlumnos {
-			public static void main(String[] args) {
-			System.out.println("Tablas de Multiplicar");
+	
+	public static void main(String[] args) {
+		System.out.println("Tablas de Multiplicar");
+		for(int i = 1; i <= 10; i++) {
+			for(int o = 0; o <= 10; o++) {
+				System.out.println(i + " x " + o + " = " + (i*o));
+			}
 		}
 	}
-
-
+			
+}
