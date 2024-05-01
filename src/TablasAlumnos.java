1c001dc168887a14eeb00dfcd3cde5c379cffaac
luisj <luisj@LuisPc>
2024-05-01 14:07:37 +0200

    Actualización de la tabla realizada hasta al 10 x 10

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,25 @@
 
 public class TablasAlumnos {
 			public static void main(String[] args) {
-			System.out.println("Tablas de Multiplicar");
-		}
-	}
+			 System.out.println("Tablas de Multiplicar");
+			 
+			 // Ciclo para las tablas del 1 al 10
+			 	for (int i = 1; i <= 10; i++) {
+			 System.out.println("Tabla de multiplicar del " + i + ":");
+			 
+			 	// Ciclo para cada tabla hasta el 10 x 10
+			 		for (int j = 1; j <= 10; j++) {
+			 		
+			 		System.out.println(i + " x " + j + " = " + (i * j));
+			 		
+			        }
+			 	
+			 //Para generar un espacio entre las tablas
+			 System.out.println("");
+			 
+			 	}
+			 }
+}
+
 
 
