1f11aa6fd136548469cb737675de27ea6ea7b41b
EricJoseph04 <166606801+EricJoseph04@users.noreply.github.com>
2024-05-02 12:12:24 +0200

    Update TablasAlumnos.java
    Modificacion

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,13 @@
-
 public class TablasAlumnos {
-			public static void main(String[] args) {
-			System.out.println("Tablas de Multiplicar");
-		}
-	}
-
+    public static void main(String[] args) {
+        for (int numeroDeTabla = 1; numeroDeTabla <= 10; numeroDeTabla++) {
+            System.out.println("Tabla de multiplicar del " + numeroDeTabla + ":");
+            for (int i = 1; i <= 12; i++) {
+                int resultado = numeroDeTabla * i;
+                System.out.printf("%d x %d = %d%n", numeroDeTabla, i, resultado);
+            }
+            System.out.println(); // Agrega una línea en blanco entre las tablas
+        }
+    }
+}
 
