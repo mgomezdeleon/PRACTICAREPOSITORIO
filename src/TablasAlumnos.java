9f95399b364b09aa86cc7cb078eade4a6e6280b1
FelixJLFan <166606891+FelixJLFan@users.noreply.github.com>
2024-05-02 12:08:20 +0200

    Update TablasAlumnos.java
    Modificacion

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,19 @@
 
 public class TablasAlumnos {
-			public static void main(String[] args) {
-			System.out.println("Tablas de Multiplicar");
+	public static void main(String[] args) {
+		System.out.println("Tablas de Multiplicar");
+		imprimirTablas();
+	}
+
+	public static void imprimirTablas() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Tabla del " + i + ":");
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + " x " + j + " = " + (i * j));
+			}
+			System.out.println(); // Agrega una línea en blanco entre cada tabla
 		}
 	}
+}
 
 
