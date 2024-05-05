a1a6de9f4c08501ba2afacb3da9a5e5936a1ec46
David <David@LAPTOP-PSG6E8DL>
2024-05-05 20:30:41 +0200

    Inicialización

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -3,17 +3,7 @@ public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
 			
-			public static void main(String[] args) {
-				System.out.println("Tablas de Multiplicar");
-				
-				for (int i = 1; i <= 10; i++) {
-					System.out.println("Tabla del " + i + ":");
-					for (int j = 1; j <= 10; j++) {
-						System.out.println(i + " x " + j + " = " + (i * j));
-					}
-				
-			}
-				}
+			
 		}
 	}
 
