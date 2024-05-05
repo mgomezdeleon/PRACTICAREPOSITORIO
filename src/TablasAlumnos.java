44b481b33e449994868f2eb4efdcd0d7f41f043e
ticha <ticha@192.168.1.132>
2024-05-05 20:12:08 +0200

    Tablas de multiplicar

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -2,7 +2,14 @@
 public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
+			
+			for (int i = 1; i <= 10; i++) {
+				System.out.println("Tabla del " + i + ":");
+				for (int j = 1; j <= 10; j++) {
+					System.out.println(i + " x " + j + " = " + (i * j));
+				}
+			
 		}
-	}
+			}
 
-
+}
