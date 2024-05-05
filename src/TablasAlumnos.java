869ae2f606955e7840ce6375bf88ebb50d174f6c
mserna <mserna@LAP0041.gruposiglo.local>
2024-05-05 14:15:02 -0500

    Actualizacion de Codigo Tabla de Multiplicar

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,20 @@
 
-public class TablasAlumnos {
+	public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
-		}
+			System.out.println("Miguel Serna");
+			imprimirTablasDeMultiplicar(10);
+	}
+	public static void imprimirTablasDeMultiplicar(int hasta) {
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println("Tabla del " + i + ":");
+	            for (int j = 1; j <= hasta; j++) {
+	                System.out.println(i + " x " + j + " = " + (i * j));
+	            }
+	            System.out.println(); // Separador entre tablas
+	        }
+	 }
+			
 	}
 
 
