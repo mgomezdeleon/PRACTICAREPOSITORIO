fb3482dee93d288ddfce9a1723d63307f1fbb4d4
Derek <166606878+DerekJacqui@users.noreply.github.com>
2024-05-05 21:30:11 +0200

    Update TablasAlumnos.java

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,22 @@
 
 public class TablasAlumnos {
-			public static void main(String[] args) {
-			System.out.println("Tablas de Multiplicar");
+	public static void main(String[] args) {
+		System.out.println("Tablas de Multiplicar");
+		for(int i=0;i<11;i++){
+				System.out.println("\t---Tabla del "+i+"---");
+				System.out.println(i+"x0="+(i*0));
+				System.out.println(i+"x1="+(i*1));
+				System.out.println(i+"x2="+(i*2));
+				System.out.println(i+"x3="+(i*3));
+				System.out.println(i+"x4="+(i*4));
+				System.out.println(i+"x5="+(i*5));
+				System.out.println(i+"x6="+(i*6));
+				System.out.println(i+"x7="+(i*7));
+				System.out.println(i+"x8="+(i*8));
+				System.out.println(i+"x9="+(i*9));
+				System.out.println(i+"x10="+(i*10));
 		}
 	}
+}
 
 
