294e64a86199a541ecc0c21b53283bf2673e0152
1lima <167064605+1lima@users.noreply.github.com>
2024-05-07 13:43:54 +0200

    Update TablasAlumnos.java
    Modificacion

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -2,7 +2,17 @@
 public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
+			imprimirTablas();
+			}
+
+			public static void imprimirTablas() {
+				for (int i = 1; i <= 10; i++) {
+					System.out.println("Tabla del " + i + ":");
+					for (int j = 1; j <= 10; j++) {
+						System.out.println(i + " x " + j + " = " + (i * j));
+					}
+					System.out.println(); // Agrega una línea en blanco entre cada tabla
+				}
+			
 		}
 	}
-
-
