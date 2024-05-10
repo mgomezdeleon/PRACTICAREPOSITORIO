cb8bc8bc9c9e2e2b3e3001e9e9e61e74b385a2df
aleja <aleja@LAPTOP-IMPQVJ86>
2024-05-10 08:46:47 +0200

    Actualizacion

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -2,7 +2,13 @@
 public class TablasAlumnos {
 			public static void main(String[] args) {
 			System.out.println("Tablas de Multiplicar");
+		for(int j=0;j<11; j++) {	
+			for(int i=0;i<11;i++) {
+				
+			System.out.println(i*j);	
+				
+			}
 		}
 	}
-
+}
 
