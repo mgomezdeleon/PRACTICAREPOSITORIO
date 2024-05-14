576a7ef5d14be246d96ad3d6aad756d44ebdf2f9
guill <guill@GUILLE>
2024-05-14 10:36:17 +0200

    Practica Repositorio

--- src/TablasAlumnos.java
+++ src/TablasAlumnos.java
@@ -1,8 +1,19 @@
 
 public class TablasAlumnos {
-			public static void main(String[] args) {
-			System.out.println("Tablas de Multiplicar");
-		}
-	}
+	
+    public static void main(String[] args) {
+        printTablasDeMultiplicar();
+    }
+
+    public static void printTablasDeMultiplicar() {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Tablas De Multiplicar para " + i + ":");
+            for(int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
+}
 
 
